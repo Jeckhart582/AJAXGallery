3bd3478226f38d6fdb46bbe212eab7c486a1ae36
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-01-13 16:59:47 -0600

    Another push, added search button functionality

--- assets/java/java.js
+++ assets/java/java.js
@@ -1,3 +1,4 @@
+
 var topics = [
     "Spider-Man",
     "Thor",
@@ -7,9 +8,10 @@ var topics = [
     "Black Widow"]
 
 function generateButtons() {
+    $("#buttons").empty();
     for (var i = 0; i < topics.length; i++) {
         var newButton = $("<button>")
-        newButton.addClass("seachButton");
+        newButton.addClass("characterButton");
         newButton.html(topics[i]);
         newButton.attr("character", topics[i])
         $("#buttons").append(newButton);
@@ -17,13 +19,11 @@ function generateButtons() {
     }
 }
 
-generateButtons();
-
-
-$("button").on("click", function () {
+function generateGifs() {
+    console.log("Hello")
     var character = $(this).attr("character");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    character + "&api_key=dc6zaTOxFJmzC&limit=10";
+        character + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -39,20 +39,59 @@ $("button").on("click", function () {
                     var imageURL = results[i].images.fixed_height_still.url;
                     var p = $("<p>").text("Rating: " + rating);
                     var characterImage = $("<img>");
-                    characterImage.addClass("image")
+                    characterImage.addClass("giphy")
                     characterImage.attr("src", imageURL);
-                    characterImage.attr("alt", "character image");
+                    characterImage.attr("alt", character);
+                    gifDiv.attr("id", "giphy")
                     gifDiv.append(p);
                     gifDiv.append(characterImage)
                     $("#images").prepend(gifDiv);
                 }
             }
         })
-})
+}
 
-$("image").on("click", function() {
-    console.log("hello");
-})
+
+$("#search").on("click", function (event) {
+    event.preventDefault();
+
+    // This line grabs the input from the textbox
+    var search = $("#search-input").val().trim();
+
+    // Adding the movie from the textbox to our array
+    topics.push(search);
+    console.log(search);
+
+    // Calling renderButtons which handles the processing of our movie array
+    generateButtons();
+});
+function playGif () {
+    var character = $(this).attr("alt");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        character + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            
+            var results = response.data;
+            console.log("hello");
+            var imageURL = response.data.image_original_url;
+            $(this).attr("src", $(this).attr(imageURL));
+
+
+        })
+}
+
+$(document).on("click", ".characterButton", generateGifs);
+$(document).on("click", ".giphy", playGif);
+
+generateButtons();
+
+
 
 
 // $("#spiderman-button").on("click", function () {
